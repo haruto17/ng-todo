599e97df0c6b98c0a17f18a9749a0b1d50c3e546
haruto17 <s1902272@sendai-nct.jp>
2024-03-20 21:51:13 +0900

    リスト要素にアイコンの追加

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,11 @@
 import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, MatIconModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
